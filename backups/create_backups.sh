c7b8f8c5a6b30d6a3e1e52e0ce5a48662880658f
Kussi <sakkenino@gmail.com>
2023-07-10 15:31:05 +0300

    Add backups script

---
+++ backups/create_backups.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Defaults
+declare -A ALL
+crypted=()
+BACKUP_DIR="$HOME/backups/"
+CU='\033[0;32m' # Colour for Update (green)
+CC='\033[0;31m' # Colour for Crypted (red)
+NC='\033[0m' # No Colour
+
+#source $XDG_CONFIG_HOME/backups.conf
+# At this point I was too exhausted to start investigating why I don't have that set
+source $HOME/.config/backups.conf
+
+
+
+read -sp "password: " passwd && printf "\n"
+
+for name in "${!ALL[@]}"
+do
+
+    # turn e.g. "/etc/fstab;/etc/rc.conf" into
+    # array ("/etc/fstab" "/etc/rc.conf")
+    IFS=";" read -r -a files <<< "${ALL[$name]}"
+
+    #declare regex_name="\<${name}\>"
+
+    #if [[ " ${crypted[@]} " =~ " ${regex_name} " ]] ; then
+    if [[ " ${crypted[@]} " == *" $name "* ]] ; then
+	    (cd $BACKUP_DIR && zip -qureP $passwd $name.zip ${files[@]}) \
+	        && printf "${CU}$name: ${files[*]} ${CC}(crypted)${NC}\n" \
+	        || printf "$name: ${files[*]} ${CC}(crypted)${NC}\n"
+    else
+	    (cd $BACKUP_DIR && zip -qur $name.zip ${files[@]}) \
+	        && printf "${CU}$name: ${files[*]}${NC}\n" \
+	        || printf "$name: ${files[*]}\n"
+    fi
+done
+
